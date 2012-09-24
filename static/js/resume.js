b668cea40536b6a7a7fc4e34107a314bd88279b0
Bhargav Nookala <nooknb@gmail.com>
2012-09-24 01:52:38 -0700

    Dynamic modals

--- static/js/resume.js
+++ static/js/resume.js
@@ -4,4 +4,50 @@ $(document).ready(function () {
 		title: 'Tweets',
 		placement: 'bottom'
 	});
+
+	$('html').delegate('.location', 'hover', function () {
+		var container = $(this);
+		var mapContainer = [
+			'<div id="map" data-lat=',
+			container.data('lat'),
+			' data-long=',
+			container.data('long'),
+			'></div>'
+		].join('');
+
+		container.popover({
+			html: true,
+			content: mapContainer,
+			title: container.data('map-title'),
+			placement: 'bottom'
+		});
+		container.popover('show');
+	});
+
+	$('html').delegate('.popover', 'DOMNodeInserted', function () {
+		var container = $(this);
+		if (container.find('.leaflet-container').length === 0) {
+			var mapContainer = $('#map');
+			var mapLat = parseFloat(mapContainer.data('lat'));
+			var mapLong = parseFloat(mapContainer.data('long'));
+			var map = L.map(
+				'map',
+				{
+					dragging: false,
+					touchZoom: false,
+					scrollWheelZoom: false,
+					doubleClickZoom: false,
+					boxZoom: false,
+					trackResize: false,
+					worldCopyJump: false,
+					zoomControl: false,
+					attributionControl: false,
+				}
+			);
+			map.setView([mapLat, mapLong], 10);
+			L.tileLayer('http://{s}.tile.cloudmade.com/531cdd7137d247de8a2f7a635f2a869c/997/256/{z}/{x}/{y}.png', {
+			}).addTo(map);
+			L.marker([mapLat, mapLong]).addTo(map);
+		}
+	});
 });
